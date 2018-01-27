99e2dae9f3eddd12f07ddcdb01fb9dd37c772acc
KJackson50 <KJackson5053@gmail.com>
2018-01-27 11:24:05 -0500

    Update_2

--- PalNumbers.java
+++ PalNumbers.java
@@ -1,74 +1,82 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class PalNumbers {
 
 	public static void main(String[] args) {
-		
+
 		int highestPalNumber;
-		
-		highestPalNumber = getPal();
-		
+		List<Integer> products;
+
+		products = multiply();
+		highestPalNumber = getPal(products);
+
 		System.out.println("The highest Palindrome in this range is " + highestPalNumber);
 
 	}
-	
-	public static int getPal() {
-		int palTotal = 0;
-		int lowest = 10000;
-		int highest = 998001;
-		List <Integer> list = new ArrayList<Integer>();
-		
-		
-		
-		for(int i=lowest; i <= highest; i++) {
-			
-			String s = String.valueOf(i);
-			int length = s.length();
-			//System.out.println(length);
-			
-			if(length == 5) {
-				if(s.charAt(1) == s.charAt(3) && s.charAt(0) == s.charAt(4)) {
-					list.add(i);
-					System.out.println(i + " is a palindrome!");
-					
-				}
-				else;
-				
-			}
-			
-			if(length == 6) {
-				if(s.charAt(2) == s.charAt(3) && s.charAt(1) == s.charAt(4) && s.charAt(0) == s.charAt(5)) {
-					list.add(i);
-					System.out.println(i + " is a palindrome!");
-					
-				}
-				else;
-				
-			}
-			
 
-			
-			
+	public static int getPal(List<Integer> products) {
+		int palTotal = 0;
+		int prodElement;
+		List<Integer> list = new ArrayList<Integer>();
+
+		for (int i = 0; i <= products.size()-1; i++) {
+			prodElement = products.get(i);
+
+			String s = String.valueOf(prodElement);
+			int length = s.length();
+			// System.out.println(length);
+
+			if (length == 5) {
+				if (s.charAt(1) == s.charAt(3) && s.charAt(0) == s.charAt(4)) {
+					list.add(i);
+					// System.out.println(i + " is a palindrome!");
+
+				} else
+					;
+
+			}
+
+			if (length == 6) {
+				if (s.charAt(2) == s.charAt(3) && s.charAt(1) == s.charAt(4) && s.charAt(0) == s.charAt(5)) {
+					list.add(i);
+					// System.out.println(i + " is a palindrome!");
+
+				} else
+					;
+
+			}
+
 		}
-		
-		//E e = list.get(list.size() - 1);
-		palTotal = list.get(list.size() -1);
-		
-		
-		
-		
+		//here it's returning the index of this list, I will need to return the element
+		//instead. Do the same thing as you did above with prodElement
+		palTotal = list.size() - 1;
+
 		return palTotal;
-		
+
 	}
-	
-	//NEED TO CREATE A FUNCTION THAT MULTIPLIES ALL NUMBERS BETWEEN 100 - 999
-	//AGAINST EACHOTHER, AND RETURNS AN ARRAY OF THOSE PRODUCTS
-	
-	//One example, we can put the range of 100 - 999 in an array, then multiply 100 - 999 against it
-	//will need to find a way to iterate 100*array, then store those products in a separate array
-	//once 100 is done multiplying, go on to 101, but make sure 101 doesn't multiply into 100 again
-	//so on and so forth until the end.
-	
+
+	public static List<Integer> multiply() {
+		List<Integer> products = new ArrayList<Integer>();
+		List<Integer> range = new ArrayList<Integer>();
+
+		for (int i = 100; i <= 999; i++) {
+
+			range.add(i);
+
+		}
+		for (int i = 100; i <= 999; i++) {
+			for (int x = range.get(0); x <= range.size(); x++) {
+				products.add(i * x);
+
+			}
+		}
+
+		// System.out.println(products);
+
+		return products;
+
+	}
 
 }
